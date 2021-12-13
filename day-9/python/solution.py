aa488a6ab60151e279a3b7d7e3311c9fa36e00d0
Alex Ungurianu <alexandru.ungurianu@gmail.com>
2021-12-13 18:44:16 +0000

    Add solution to part 2 of day 9

--- day-9/python/solution.py
+++ day-9/python/solution.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 with open("../input") as f:
     board = [ [ int(c) for c in line.strip()] for line in f.readlines()]
 
@@ -15,12 +18,20 @@ R, C = len(board), len(board[0])
 def is_valid(rowIdx, colIdx):
     return 0 <= rowIdx < R and 0 <= colIdx < C
 
-def get_local_risk(board, rowIdx, colIdx):
-    is_min = True
-    here = board[rowIdx][colIdx]
+def get_neighbours(pos):
+    rowIdx, colIdx = pos
     for dR, dC in dirs:
         oR, oC = rowIdx + dR, colIdx + dC
-        if is_valid(oR, oC) and not here < board[oR][oC]:
+        if is_valid(oR, oC):
+            yield (oR, oC)
+
+
+def get_local_risk(board, pos):
+    is_min = True
+    rowIdx, colIdx = pos
+    here = board[rowIdx][colIdx]
+    for oR, oC in get_neighbours(pos):
+        if not here < board[oR][oC]:
             is_min = False
 
     if is_min:
@@ -31,6 +42,32 @@ total_risk = 0
 
 for rIdx in range(R):
     for cIdx in range(C):
-        total_risk += get_local_risk(board, rIdx, cIdx)
+        total_risk += get_local_risk(board, (rIdx, cIdx))
 
 print(total_risk)
+
+def find_basins(board):
+    visited = set()
+
+    def fill_basin(pos):
+        basin_visited = set()
+        q = [pos]
+        while q:
+            cPos = q.pop(0)
+            basin_visited.add(cPos)
+            for nPos in get_neighbours(cPos):
+                if nPos not in basin_visited and board[nPos[0]][nPos[1]] != 9:
+                    q.append(nPos)
+        
+        visited.update(basin_visited)
+        return len(basin_visited)
+
+    for rowIdx in range(R):
+        for colIdx in range(C):
+            pos = (rowIdx, colIdx)
+
+            if pos not in visited and board[rowIdx][colIdx] != 9:
+                yield fill_basin(pos)
+
+
+print(reduce(lambda acc, x: acc * x, sorted(list(find_basins(board)))[-3:]))
